fc7445099d3cf354fc337d6181ce44b22a1ce31a
gakeez <531569313@qq.com>
2020-07-01 10:41:43 +0800

    完成基本配置和首页
    下步做大转盘

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,63 @@
 //index.js
 //获取应用实例
+//通过getApp获取全局对象，然后进行全局变量和全局方法的使用。
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World1234',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    components: [
+      {
+          title: '营销组件',
+          children: [
+              {
+                  id: 'big-wheel',
+                  url: '/pages/big-wheel/index',
+                  name: '大转盘动画',
+                  show: true
+              },
+              /* {
+                  id: 'packet-rain',
+                  url: '/pages/packet-rain/index',
+                  name: '红包雨动画',
+                  show: false
+              },
+              {
+                  id: 'grid-card',
+                  url: '/pages/grid-card/index',
+                  name: '九宫格翻牌动画',
+                  show: true
+              },
+              {
+                  id: 'slot-machine',
+                  url: '/pages/slot-machine/index',
+                  name: '老虎机动画',
+                  show: false
+              } */
+          ]
+      },
+      /* {
+          title: '业务组件',
+          children: [
+              {
+                  id: 'scoll-nav',
+                  url: '/pages/scoll-nav/index',
+                  name: '滚动导航条',
+                  show: false
+              },
+              {
+                id: 'share-sheet',
+                url: '/pages/painter/index',
+                name: '微信分享组件(转发好友/分享图)',
+                show: false
+              },
+              {
+                id: 'float-icon',
+                url: '/pages/float-icon/index',
+                name: '悬浮按钮',
+                show: true
+              }
+          ]
+      }, */
+  ],
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
