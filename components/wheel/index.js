c04c43adc6881d85368dff99d2c153a949243b9f
gakeez <531569313@qq.com>
2020-07-01 16:15:46 +0800

    完成图片版大转盘

---
+++ components/wheel/index.js
@@ -0,0 +1,115 @@
+Component({
+  externalClasses: ['sol-class'],
+  properties: {
+      // 划分区域
+      areaNumber: {
+          type: Number,
+          value: 6
+      },
+      // 中奖区域 从1开始
+      awardNumer: {
+          type: Number,
+          value: 1
+      },
+      // 速度
+      speed: {
+          type: Number,
+          value: 16
+      },
+      // 抽奖模式:1:盘转,2:指针旋转
+      mode: {
+          type: Number,
+          value: 2,
+          observer(newVal, oldVal) {
+              // 切换模式的时候重置
+              this.setData({
+                  deg: 0
+              })
+          }
+      }
+  },
+  data: {
+      deg: 0,
+      singleAngle: '', // 每片扇形的角度
+      isStart: false
+  },
+  methods: {
+      // 初始化转盘分区
+      init() {
+          let { areaNumber } = this.data
+          const singleAngle = 360 / areaNumber
+          this.setData({
+              singleAngle: singleAngle
+          })
+      },
+      // 点击开始抽奖
+      start() {
+          // 在对组件进行封装时 在当前页面想要获取组件中的某一状态，需要使用到this.triggerEvent(' ',{},{}),第一个参数是自定义事件名称，这个名称是在页面调用组件时bind的名称，第二个对象就可以将想要的属性拿到，第三个参数是事件选项
+          // 让父组件监听start
+          this.triggerEvent('start')
+      },
+      begin() {
+          // 点击开始抽奖
+          let { deg, awardNumer, singleAngle, speed, isStart, mode } = this.data
+          /* 如果isStart为true，则表示正在抽奖，直接中断 */
+          if (isStart) return
+          // 执行前，用isStart上锁
+          this.data.isStart = true
+          let endAddAngle = 0
+          if (mode == 2) {
+              endAddAngle = 360 - ((awardNumer - 1) * singleAngle + singleAngle / 2) // 中奖角度
+          } else {
+              endAddAngle = (awardNumer - 1) * singleAngle + singleAngle / 2 + 360 // 中奖角度
+          }
+          const rangeAngle = (Math.floor(Math.random() * 4) + 4) * 360 // 随机旋转几圈再停止
+          console.log('中奖角度：'+endAddAngle)
+          console.log('随机转圈总角度：'+rangeAngle)
+          let cAngle
+          deg = 0
+          /* setInterval() 方法可按照指定的周期（以毫秒计）来调用函数或计算表达式。etInterval() 方法会不停地调用函数，直到 clearInterval() 被调用或窗口被关闭。 */
+          this.timer = setInterval(() => {
+              // 如果deg小于任意旋转的角度，则增加speed
+              if (deg < rangeAngle) {
+                  deg += speed
+                  console.log('当前旋转角度：'+deg)
+              } else {
+                  /* deg大于任意旋转角度后，则开始减速 */
+                  cAngle = (endAddAngle + rangeAngle - deg) / speed
+                  console.log('cAngle：'+cAngle)
+                  // 如果cAngle比原来的speed还快，就设置为speed；如果比speed慢，比1快就设置cAngle；如果比1慢则设为1
+                  cAngle = cAngle > speed ? speed : cAngle < 1 ? 1 : cAngle
+                  console.log('cAngle：'+cAngle)
+                  deg += cAngle
+                  console.log('当前旋转角度：'+deg)
+                  /* 如果旋转大于总旋转角度（中奖角度+随机转圈角度），则停止 */
+                  if (deg >= endAddAngle + rangeAngle) {
+                      /* 角度为中奖角度+随机转圈角度 */
+                      deg = endAddAngle + rangeAngle
+                      console.log('当前旋转角度：'+deg)
+                      // 解锁
+                      this.data.isStart = false
+                      // 终止setInterval()
+                      clearInterval(this.timer)
+                      // 触发成功的函数
+                      this.triggerEvent('success')
+                  }
+              }
+              this.setData({
+                  singleAngle,
+                  deg,
+                  mode
+              })
+          }, 1000 / 60)
+      }
+  },
+
+  //attached() 在组件实例进入页面节点树时执行
+  attached() {
+      this.init()
+  }
+
+  /* 
+      页面和组件初始化执行顺序
+      ①created（组件）> ②attached（组件）> ③onLoad（页面）> ④onShow（页面）> ⑤ready（组件）> ⑥onReady（页面） 
+  */
+})
--- pages/big-wheel/index.js
+++ pages/big-wheel/index.js
@@ -1,66 +1,69 @@
 // pages/big-wheel/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+      award: 1,
+      mode: 2, // 旋转模式
+      awardList: [
+          { title: '10个金币' },
+          { title: '20个金币' },
+          { title: '30个金币' },
+          { title: '50个金币' },
+          { title: '80个金币' },
+          { title: '200个金币' }
+      ] // 顺时针对应每个奖项
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {},
+  // 用户点击开始抽奖
+  wheelStart() {
+      // 设置奖项
+      this.setData({
+          /* Math.floor 返回小于或等于一个给定数字的最大整数 */
+          /* Math.random 返回介于 0（包含） ~ 1（不包含） 之间的一个随机数： */
+          award: Math.floor(Math.random() * 6 + 1) //生成1到6随机。注意：安全起见生成奖项应该由后端完成
+          // 个人觉得可以不加1
+      })
+      
+      // 触发组件开始方法
+      /* 可在父组件里调用 this.selectComponent ，获取子组件的实例对象。（插件的自定义组件将返回 null） */
+      this.selectComponent('#wheel').begin()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 抽奖完成后操作
+  wheelSuccess() {
+      /* 获得中奖区域的序号 */
+      const index = this.data.award - 1
+      console.log('bind:success', this.data.awardList[index])
+      /* 显示消息提示框 */
+      wx.showToast({
+          title: `恭喜你获得${this.data.awardList[index].title}`,
+          icon: 'none'
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 切换模式
+  switchMode(e) {
+      const { type } = e.currentTarget.dataset
+      this.setData({
+          mode: type
+      })
   },
+  /* 转发*/
+  /* 
+      在页面的js文件中定义了 onShareAppMessage 函数时，页面可以表示改页面可以转发。可以在函数中设置页面转发的信息。
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+          1. 只有定义了该函数，小程序右上角的菜单中才会有转发按钮
+          2. 用户点击转发按钮的时候回调用该函数
+          3. 该函数内需要 return 一个 Object，Object中包含转发的信息（可自定义转发的内容）
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  页面中有可以触发转发时间的地方有两个：
+　　    1. 一个是右上角菜单中的转发按钮
+　　    2. 另一个是页面中具有属性open-type且其值为share的button。（注：必须是button组件，其他组件中设置 open-type="share" 无效）
+  */
+  /* onShareAppMessage: function(ops) {
+      return {
+          title: '大转盘组件',
+          path: '/pages/big-wheel/index'
+      }
+  } */
 })
